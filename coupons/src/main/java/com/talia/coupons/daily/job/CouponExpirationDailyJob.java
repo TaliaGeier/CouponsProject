feed9eaa2562ff68dd848bedb16d706b0a59a179
TaliaGeier <43821480+TaliaGeier@users.noreply.github.com>
2019-04-29 11:25:13 +0300

    java

--- coupons/src/main/java/com/talia/coupons/daily/job/CouponExpirationDailyJob.java
+++ coupons/src/main/java/com/talia/coupons/daily/job/CouponExpirationDailyJob.java
@@ -1,57 +1,33 @@
 package com.talia.coupons.daily.job;
 
 import java.util.ArrayList;
+import java.util.TimerTask;
 
 import com.talia.coupons.dao.CouponsDao;
+import com.talia.coupons.dao.PurchasesDao;
+import com.talia.coupons.exceptions.ApplicationException;
 
-public class CouponExpirationDailyJob implements Runnable {
+public class CouponExpirationDailyJob  extends TimerTask {
 
 	
+	private CouponsDao couponsDao;
+	private PurchasesDao purchasesDao;
+
+	//---------------------CTOR----------------------
 	public CouponExpirationDailyJob() {
+		this.couponsDao = new CouponsDao();
+		this.purchasesDao = new PurchasesDao();
 	}
-	
-	
+
 	@Override
 	public void run() {
-
-		while (true) {
-			CouponsDao CouponsDBDAO = new CouponsDao();
-			System.out.println("DailyJob is starting");
-			try {
-				synchronized (CouponsDBDAO.getAllExpiredCouponsById()) {
-					ArrayList<Long> list = CouponsDBDAO.getAllExpiredCouponsById();
-					while (list.size() > 0) {
-						CouponsDBDAO.deleteAllCouponPurchasesByCouponId(list.get(0));
-						CouponsDBDAO.deleteCoupon(list.get(0));
-						list.remove(0);
-						System.out.println("coupons deleted");
-					}
-				}
-			} catch (Exception e) {
-				System.out.println(e);
-			}
-
-			try {
-				Thread.sleep(1000 * 60 * 60 * 24);
-			} catch (InterruptedException e) {
-				System.out.println(e);
-			}
-		}
-
-	}
-
-	/**
-	 * stop - stops the thread.
-	 */
-	public void stop() {
-		
-		System.out.println("Daily job has stopped");
-
 		try {
-			wait();
-		} catch (InterruptedException e) {
-			System.out.println(e);
+			couponsDao.deleteAllExpiredCoupons();
+			purchasesDao.deleteExpiredPurchases();
+		} catch (ApplicationException e) {
+			e.printStackTrace();
 		}
-	}
+	
+	}	
 
 }
