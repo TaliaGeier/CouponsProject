17f34ab0dc027e75a210782d99bfb8974fec88bd
TaliaGeier <43821480+TaliaGeier@users.noreply.github.com>
2019-07-03 14:42:22 +0300

    java

--- CouponsProject/src/app/shared/models/UserLoginDetails.ts
+++
@@ -1,9 +0,0 @@
-export class UserLoginDetails {
-    public constructor( public userEmail?: string, 
-                        public password?: string,
-                        public type?
-                        )
-        {
-
-        }
-}
